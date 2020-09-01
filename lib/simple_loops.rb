da27fb590e22443fb51e85d373f2d995a4a60306
Jacqueline <lojacqueline2@gmail.com>
2020-09-01 03:08:26 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -29,12 +29,10 @@ def output_array(array)
 end
 
 
-def return_string_array (array)
-  
-array = [5, 4, 3, 2, 1]
-array.each do |num|
-  
-puts num.to_s
-
-end
+def return_string_array(array)
+  count = 0
+  while count < array.length do
+    array[count].to_s
+    count += 1
+  end
 end
